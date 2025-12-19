d5e0f71e8ffca1458cc134b82b2cd6e31f04ed92
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-12-19 13:47:09 -0400

    Crear entidad cliente

---
+++ InventoryManagement/InventoryManagement/Models/cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionInventario.Models
+{
+    public class cliente
+    {
+        public int clienteId { get; set; }
+        public string? NombreCliente { get; set; }
+        public string? RNCCliente { get; set; }
+        public string? TelefonoCliente { get; set; }
+        public string? TipoCliente { get; set; }
+    }
+}
