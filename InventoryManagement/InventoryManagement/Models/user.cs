6f662eef68de206cea56a3e8d738e692199f6ed6
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-12-19 13:48:38 -0400

    Crear entidad user

---
+++ InventoryManagement/InventoryManagement/Models/user.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionInventario.Models
+{
+    public class user
+    {
+        [Key]
+        public int userId { get; set; }
+        public string? userName { get; set; }
+        public string? password { get; set; }
+        public string? rol { get; set; }
+    }
+}
