4a47a01756df3910bddc7f2e7a9a9802f080d6f4
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-12-19 13:47:18 -0400

    Crear entidad compra

---
+++ InventoryManagement/InventoryManagement/Models/compra.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionInventario.Models
+{
+    public class compra
+    {
+        [Key]
+        public int compraId { get; set; }
+        public int proveedorId { get; set; }
+        public string? NumeroFactura { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime fechaCompra { get; set; }
+        public string? metodoPago { get; set; }
+        public string? tipoEntrega { get; set; }
+        public string? transportista { get; set; }
+        public string? lugarRecepcion { get; set; }
+        public double costoEnvio { get; set; }
+        public double subTotal { get; set; }
+        public double impuesto { get; set; }
+        public double descuento { get; set; }
+        public double total { get; set; }
+        public string? almacenDestino { get; set; }
+        public string? estado { get; set; } 
+        public string? motivoAnulacion { get; set; }
+        public string? regionCompra { get; set; }
+    }
+}
