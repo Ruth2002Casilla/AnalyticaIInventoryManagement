95a2992fcf9adeb2f0757da75cac1540341a3d39
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-12-19 13:46:54 -0400

    Crear entidad categoria

---
+++ InventoryManagement/InventoryManagement/Models/categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionInventario.Models
+{
+    public class categoria
+    {
+        [Key]
+        public int categoriaId { get; set; }
+        public string? nombreCategoria { get; set; }
+        public int productoId { get; set; }
+    }
+}
