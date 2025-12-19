40504a2959e95942de313993a298df5db5b32551
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-12-19 13:47:29 -0400

    Crear entidad detalleCompra

---
+++ InventoryManagement/InventoryManagement/Models/detalleCompra.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionInventario.Models
+{
+    public class detalleCompra
+    {
+        [Key]
+        public int detalleCompraId { get; set; }
+        public int compraId { get; set; }
+        public int productoId { get; set; }
+        public string? unidadMedida { get; set; }
+        public double costoUnitario { get; set; }
+        public double impuestoUnitario { get; set; }
+        public double descuentoUnitario { get; set; }
+        public double totalLinea { get; set; }
+        public string? calidad { get; set; }
+        public string? certificaciones { get; set; }
+    }
+}
