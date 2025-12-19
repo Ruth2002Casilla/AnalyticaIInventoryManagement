6ef8b7df4bc04132affe7993afc5431008fea3b1
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-12-19 13:47:41 -0400

    Crear entidad detalleVenta

---
+++ InventoryManagement/InventoryManagement/Models/detalleVenta.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionInventario.Models
+{
+    public class detalleVenta
+    {
+        [Key]
+        public int detalleVentaId { get; set; }
+        public int ventaId { get; set; }
+        public int productoId { get; set; }
+        public double precioUnitario { get; set; }
+        public double descuentoUnitario { get; set; }
+        public double impuestoUnitario { get; set; }
+        public double totalLinea { get; set; }
+        public string? unidadMedida { get; set; }
+        public string? ubicacionAlmacen { get; set; }
+        public string? promocionAplicada { get; set; }
+        public double margenGanancia { get; set; }
+    }
+}
+
+
+
+
